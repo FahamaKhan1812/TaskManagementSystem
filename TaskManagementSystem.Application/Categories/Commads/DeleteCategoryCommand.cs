85220a1231babb0ca322775c83a143747367e2a8
Fahama Khan <fahamakhan990@gmail.com>
2023-11-12 23:10:43 +0500

    add logic for delete category

---
+++ TaskManagementSystem.Application/Categories/Commads/DeleteCategoryCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using TaskManagementSystem.Application.Models;
+
+namespace TaskManagementSystem.Application.Categories.Commads;
+public class DeleteCategoryCommand : IRequest<OperationResult<string>>
+{
+    public Guid CategoryId { get; set; }
+}
