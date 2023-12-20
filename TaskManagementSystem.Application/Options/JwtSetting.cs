00eee43844bf51d364fba1e1550df15eb67bb5c3
Fahama Khan <fahamakhan990@gmail.com>
2023-12-21 00:08:47 +0500

    add JWTSetting class

---
+++ TaskManagementSystem.Application/Options/JwtSetting.cs
@@ -0,0 +1,7 @@
+﻿namespace TaskManagementSystem.Application.Options;
+public class JwtSetting
+{
+    public string Secret { get; set; }
+    public string Issuer { get; set; }
+    public string[] Audiences { get; set; }
+}
