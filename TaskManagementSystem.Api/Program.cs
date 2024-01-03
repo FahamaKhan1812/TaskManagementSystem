4df7493b1e4bcc676e8a290f4cb6695b00b24931
Fahama Khan <fahamakhan990@gmail.com>
2024-01-03 23:12:59 +0500

    add logging

--- TaskManagementSystem.Api/Program.cs
+++ TaskManagementSystem.Api/Program.cs
@@ -2,46 +2,68 @@ using TaskManagementSystem.Application.Extensions;
 using TaskManagementSystem.Api.Extensions;
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using TaskManagementSystem.Api.Options;
+using NLog.Web;
+using NLog;
+using NLog.LayoutRenderers;
+using TaskManagementSystem.Api;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-builder.Services.AddApiVersion();
-builder.Services.AddMSDb(builder.Configuration);
-builder.Services.AddIdentityDb();
-builder.Services.AddJwtAuthentication(builder.Configuration);
-builder.Services.AddApplication();
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.ConfigureOptions<ConfigureSwaggerOptions>();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(opt =>
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Add services to the container.
+
+    builder.Services.AddControllers();
+    builder.Services.AddApiVersion();
+    builder.Services.AddMSDb(builder.Configuration);
+    builder.Services.AddIdentityDb();
+    builder.Services.AddJwtAuthentication(builder.Configuration);
+    builder.Services.AddApplication();
+
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.ConfigureOptions<ConfigureSwaggerOptions>();
+
+    builder.Logging.ClearProviders();
+    builder.Host.UseNLog();
+
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
     {
-        IApiVersionDescriptionProvider provider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
-        foreach(ApiVersionDescription desc in provider.ApiVersionDescriptions)
+        app.UseSwagger();
+        app.UseSwaggerUI(opt =>
         {
-            opt.SwaggerEndpoint(
-               $"/swagger/{desc.GroupName}/swagger.json", desc.ApiVersion.ToString()
-               );
-        }
-    });
+            IApiVersionDescriptionProvider provider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+        foreach(ApiVersionDescription desc in provider.ApiVersionDescriptions)
+            {
+                opt.SwaggerEndpoint(
+                   $"/swagger/{desc.GroupName}/swagger.json", desc.ApiVersion.ToString()
+                   );
+            }
+        });
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    logger.Error(ex);
+    throw (ex);
+}
+finally
+{
+    NLog.LogManager.Shutdown();
+}
