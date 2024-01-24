109e76f010b10e9242d47e5631f5766ed84482e4
Fahama Khan <fahamakhan990@gmail.com>
2024-01-24 23:08:51 +0500

    add HttpContext Extension

---
+++ TaskManagementSystem.Api/Extensions/HttpContextExtension.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Claims;
+
+namespace TaskManagementSystem.Api.Extensions;
+public static class HttpContextExtensions
+{
+    public static string? GetUserRole(this HttpContext context)
+    {
+        // Make sure the ClaimsIdentity exists before attempting to retrieve roles
+        if (context.User.Identity is ClaimsIdentity identity)
+        {
+            var roleClaim = identity.FindFirst(ClaimTypes.Role);
+            return roleClaim?.Value;
+        }
+
+        return null;
+    }
+
+    private static Guid GetClaimValue(this HttpContext context, string key)
+    {
+        var identity = context.User.Identity as ClaimsIdentity;
+
+        // Make sure the claim exists before attempting to parse its value
+        var claim = identity?.FindFirst(key);
+
+        if (claim != null && Guid.TryParse(claim.Value, out var claimValue))
+        {
+            return claimValue;
+        }
+
+        // Handle the case where the claim is not found or cannot be parsed
+        throw new InvalidOperationException($"Claim '{key}' not found or has an invalid value.");
+    }
+}
