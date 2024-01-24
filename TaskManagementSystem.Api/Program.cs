0073f56da8ce09ee83942fb005e8baa13c999b57
Fahama Khan <fahamakhan990@gmail.com>
2024-01-24 23:08:36 +0500

    add error handling for 401

--- TaskManagementSystem.Api/Program.cs
+++ TaskManagementSystem.Api/Program.cs
@@ -6,6 +6,7 @@ using NLog.Web;
 using NLog;
 using NLog.LayoutRenderers;
 using TaskManagementSystem.Api;
+using System.Text.Json;
 
 var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
 try
@@ -51,6 +52,24 @@ try
     app.UseHttpsRedirection();
 
     app.UseAuthentication();
+    app.UseStatusCodePages(async context =>
+    {
+        if (context.HttpContext.Response.StatusCode == 401)
+        {
+            context.HttpContext.Response.ContentType = "application/json";
+
+            var errorResponse = new
+            {
+                StatusCode = 401,
+                Message = "Unauthorized. Please provide a valid JWT."
+            };
+
+            var jsonResponse = JsonSerializer.Serialize(errorResponse);
+            await context.HttpContext.Response.WriteAsync(jsonResponse);
+        }
+    });
+
+
     app.UseAuthorization();
 
     app.MapControllers();
