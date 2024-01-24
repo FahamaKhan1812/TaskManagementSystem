ebfb6be182f8a2be059f922f1a5a50f7f44de5a6
Fahama Khan <fahamakhan990@gmail.com>
2024-01-24 23:10:04 +0500

    add routes

--- TaskManagementSystem.Api/ApiRoutes.cs
+++ TaskManagementSystem.Api/ApiRoutes.cs
@@ -2,4 +2,15 @@
 public class ApiRoutes
 {
     public const string BaseRoute = "/api/v{version:apiVersion}/[controller]";
+
+    public class Identity
+    {
+        public const string RegisterNewIdentity = "register";
+        public const string LoginIdentity = "login";
+        public const string IdRoute = "{identityId}";
+    }
+    public class Tasks
+    {
+        public const string IdRoute = "{id}";
+    }
 }
